500ecc64e4f7ee69fb3aaead1deb636429eceb66
c-Aurora <171968025@qq.com>
2021-10-30 23:47:46 +0800

    Site updated: 2021-10-30 23:47:46

---
+++ js/click_show_text.js
@@ -0,0 +1,34 @@
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array
+        ("Js", "Css", "HTML", "Json", "Vue", "Node", "Webpack", "git", "Jquery", "Html5", "Css3", "react");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "rgb(" + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + "," + ~~(255 * Math.random()) + ")"
+
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+			3000,
+			function() {
+			    $i.remove();
+			});
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
